99548c870cfc25d944449013f785db787e3d48ae
Maryam-as <203218399+Maryam-as@users.noreply.github.com>
2025-11-07 12:44:40 +0330

    add controlSearchResults function with basic logic
    Added controlSearchResults function to handle search queries from the
    SearchView.
    Currently logs fetched search results to the console; rendering
    functionality will be implemented in later updates.

--- src/js/controller.js
+++ src/js/controller.js
@@ -5,8 +5,7 @@ import 'regenerator-runtime/runtime';
 
 import * as model from './model.js';
 import recipeView from './views/recipeView.js';
-
-///////////////////////////////////////
+import searchView from './views/searchView.js';
 
 // Fetch and display a recipe
 const controlRecipes = async () => {
@@ -31,6 +30,23 @@ const controlRecipes = async () => {
     recipeView.renderError();
   }
 };
+
+const controlSearchResults = async () => {
+  try {
+    const query = searchView.getQuery();
+
+    // Guard clause
+    if (!query) {
+      return;
+    }
+
+    await model.loadSearchResults(query);
+    console.log(model.state.search.results);
+  } catch (err) {
+    console.error(err);
+  }
+};
+controlSearchResults();
 
 // Pubblisher-subscriber pattern
 const init = () => {
