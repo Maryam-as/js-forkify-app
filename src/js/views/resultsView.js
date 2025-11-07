86cc9dce9710bd6a886224e6839290a019242aee
Maryam-as <203218399+Maryam-as@users.noreply.github.com>
2025-11-07 16:17:19 +0330

    set custom error message and handle empty search results
    - Added a custom `_errorMessage` in `ResultsView` for when no search
    results are found.
    - Updated `View.render` to check if data is undefined or an empty array,
     and render the error message in that case.

--- src/js/views/resultsView.js
+++ src/js/views/resultsView.js
@@ -2,7 +2,7 @@ import View from './view';
 
 class ResultsView extends View {
   _parentElement = document.querySelector('.results');
-  _errorMessage = '';
+  _errorMessage = 'No recipes found for your query! Please try again!';
   _message = '';
 
   _generateMarkup() {
