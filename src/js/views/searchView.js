8d55dd703f62958417db6e932d2d7f5dab0bfb43
Maryam-as <203218399+Maryam-as@users.noreply.github.com>
2025-11-07 12:53:38 +0330

    add addHandlerSearch method to handle form submissions
    Added the addHandlerSearch() method to the SearchView class, which listens
    for the form's submit event and prevents page reload. This enables
    triggering a custom handler function for performing searches dynamically
    via JavaScript.

--- src/js/views/searchView.js
+++ src/js/views/searchView.js
@@ -4,6 +4,14 @@ class SearchView {
   getQuery() {
     return this.#parentElement.querySelector('.search__field').value;
   }
+
+  addHandlerSearch(handler) {
+    this.#parentElement.addEventListener('submit', (event) => {
+      // Prevent form submission from reloading the page
+      event.preventDefault();
+      handler();
+    });
+  }
 }
 
 export default new SearchView();
