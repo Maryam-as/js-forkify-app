0b357a18921814c534d5121d369550095faa7d8b
Maryam-as <203218399+Maryam-as@users.noreply.github.com>
2025-11-08 22:11:40 +0330

    refactor: extract preview list items into PreviewView and enhance View.render flexibility
    - Created a new PreviewView class to handle rendering of individual
    preview list items (used by ResultsView and BookmarksView).
    - Updated the View.render() method to accept a second optional parameter
    (render = true). When set to false, the method now returns the generated
    markup instead of rendering it directly to the DOM.
    - Refactored BookmarksView and ResultsView to use previewView.render(data, false)
    for generating preview item markup, improving code reuse and maintainability.

--- src/js/views/bookmarksView.js
+++ src/js/views/bookmarksView.js
@@ -1,4 +1,5 @@
-import View from './view';
+import View from './view.js';
+import previewView from './previewView.js';
 
 class BookmarksView extends View {
   _parentElement = document.querySelector('.bookmarks__list');
@@ -7,28 +8,8 @@ class BookmarksView extends View {
 
   _generateMarkup() {
     return this._data
-      .map((bookmark) => this._generateMarkupPreview(bookmark))
+      .map((bookmark) => previewView.render(bookmark, false))
       .join('');
-  }
-
-  _generateMarkupPreview(bookmark) {
-    const id = window.location.hash.slice(1);
-
-    return `
-      <li class="preview">
-        <a class="preview__link ${
-          bookmark.id === id ? 'preview__link--active' : ''
-        }" href="#${bookmark.id}">
-          <figure class="preview__fig">
-            <img src="${bookmark.image}" alt="${bookmark.title}" />
-          </figure>
-          <div class="preview__data">
-            <h4 class="preview__title">${bookmark.title}</h4>
-            <p class="preview__publisher">${bookmark.publisher}</p>
-          </div>
-        </a>
-      </li>   
-    `;
   }
 }
 
